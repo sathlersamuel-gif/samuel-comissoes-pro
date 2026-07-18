6e424858b73d8ced4a9bbab73e12d0d0d3a203b9
sathlersamuel-gif <sathlersamuel@gmail.com>
2026-07-18 08:45:14 -0400

    Atualizar cache para correção da comissão percentual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'samuel-comissoes-pro-v42';
+const CACHE_NAME = 'samuel-comissoes-pro-v43';
 const APP_SHELL = [
   './',
   './index.html',
@@ -19,6 +19,7 @@ const APP_SHELL = [
   './script.js?v=4',
   './brand-product-selector.css?v=1',
   './brand-product-selector.js?v=1',
+  './commission-percentage-restore.js?v=1',
   './import-backup.js?v=2',
   './app-upgrades.js?v=1',
   './app-features.js?v=2',
